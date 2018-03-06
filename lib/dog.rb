a916e990d9eeb65c8d0a1204cba4be3ecf556171
Matt Zuzolo <mattzuzolo@outlook.com>
2018-03-06 00:23:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,17 @@
 
 class Dog
   
-  def initialize (name)
-    @name = name
-  end
+  # def initialize (name)
+  #   @name = name
+  # end
   
-  def name=(name)
-    @name = name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
   
-  def name
-    @name
-  end  
+  # def name
+  #   @name
+  # end  
   
   
   def bark
